ef33c9967d09c476e5815733fb2ccc700196f332
Wolfgang Schulz <wolfgang.schulz@gmail.com>
2017-12-22 22:45:53 +0100

    Initial Commit

---
+++ cylinder_opengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cylinder_opengl
+//
+//  Created by Wolfgang Schulz on 22.12.17.
+//  Copyright © 2017 EkwoTECH GmbH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
